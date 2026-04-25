927826539c716c0c63911ff79cd75cf8a8848101
duong <22520298@gm.uit.edu.vn>
2026-04-25 17:21:25 +0700

    feat(quiz): add result screen and api get quiz

--- backend/src/routes/quiz.routes.ts
+++ backend/src/routes/quiz.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { generateAndSaveQuiz } from "../controllers/quiz.controller";
+import { getUserQuizzes, generateAndSaveQuiz } from "../controllers/quiz.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post("/generate-quiz", authenticate, generateAndSaveQuiz);
+router.get("/my-quizzes", authenticate, getUserQuizzes);
 
 export default router;
