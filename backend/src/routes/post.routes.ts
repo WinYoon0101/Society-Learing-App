64c6b73d6bdb5f62139d9a354fd03faeee0fcecf
Phạm Viết Quang Huy <24520692@gm.uit.edu.vn>
2026-05-01 15:32:59 +0700

    add be fo muilti image post

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import { createPost, getFeed, deletePost } from '../controllers/post.controller';
 import { authenticate } from '../middlewares/auth.middleware'; // Ktra đăng nhập
-import { uploadMedia } from '../middlewares/upload.middleware';  // Xử lý upload ảnh
+import { uploadImages } from '../middlewares/upload.middleware';  
 
 const router = express.Router();
-router.post('/create', authenticate, uploadMedia, createPost);
+router.post('/create', authenticate, uploadImages, createPost);
 router.get('/feed', authenticate, getFeed);
 router.delete('/:id', authenticate, deletePost);
 
