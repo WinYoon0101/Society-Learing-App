821b7ff97049e5dfa6071d6ee36756a2b030de7a
Phạm Viết Quang Huy <24520692@gm.uit.edu.vn>
2026-05-01 19:32:49 +0700

    add be

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createPost, getFeed, deletePost } from '../controllers/post.controller';
-import { authenticate } from '../middlewares/auth.middleware'; // Ktra đăng nhập
+import { authenticate } from '../middlewares/auth.middleware'; // Ktra đăng nhậpg
 import { uploadImages } from '../middlewares/upload.middleware';  
 
 const router = express.Router();
 router.post('/create', authenticate, uploadImages, createPost);
 router.get('/feed', authenticate, getFeed);
 router.delete('/:id', authenticate, deletePost);
+ 
 
 export default router;
