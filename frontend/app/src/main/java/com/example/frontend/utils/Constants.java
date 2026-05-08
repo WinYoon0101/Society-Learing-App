49c9286dd1fe0273a811ae0b5bf25bc24ae63d34
PwiseV <21520392@gm.uit.edu.vn>
2026-05-08 22:17:35 +0700

    fix: uncomment socket , UI fix and constants for emulator

--- frontend/app/src/main/java/com/example/frontend/utils/Constants.java
+++ frontend/app/src/main/java/com/example/frontend/utils/Constants.java
@@ -2,14 +2,18 @@ package com.example.frontend.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://192.168.1.2:3000/api/";
-    public static final String SOCKET_URL = "http://192.168.1.2:3000";
+    public static final String BASE_URL = "http://10.0.2.2:3000/api/";
+    public static final String SOCKET_URL = "http://10.0.2.2:3000";
 }
 
 
 
-//http://10.0.2.2:3000/api/
-//http://192.168.54.22:3000/api/
-//192.168.56.1
+//--- Emulator (máy ảo Android Studio) ---
+//BASE_URL = "http://10.0.2.2:3000/api/"
+//SOCKET_URL = "http://10.0.2.2:3000"
+
+//--- Điện thoại thật (đổi IP thành IP máy tính trong LAN) ---
+//BASE_URL = "http://192.168.54.22:3000/api/"
+//SOCKET_URL = "http://192.168.54.22:3000"
 
 
